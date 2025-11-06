24f24ad39b44004754864cb90eaf23490a65aaaa
Pavlenty-great <brosok05@bk.ru>
2025-11-06 20:44:41 +0300

    Updated settings.py

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'student_dashboard',
+        'USER': 'postgres',
+        'PASSWORD': 'Dank_11774$_Piron',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
